a1751a6a05ffd4bc5ca25aa5b659082ba7bf8c97
Dan Rees-Jones <revdan@gmail.com>
2012-01-19 13:12:57 +0000

    Initial Commit

---
+++ index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+
+	<!-- Basic Page Needs
+  ================================================== -->
+	<meta charset="utf-8">
+	<title>Dan Rees&ndash;Jones | Curriculum Vit&aelig;</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!-- Mobile Specific Metas
+  ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- CSS
+  ================================================== -->
+	<link rel="stylesheet" href="stylesheets/base.css">
+	<link rel="stylesheet" href="stylesheets/skeleton.css">
+	<link rel="stylesheet" href="stylesheets/layout.css">
+	<link href="http://fonts.googleapis.com/css?family=Poly" rel="stylesheet" type="text/css">
+	
+
+	<!-- Favicons
+	================================================== -->
+	<link rel="shortcut icon" href="images/favicon.ico">
+
+
+</head>
+<body>
+
+	<div class="container">
+		<header class="sixteen columns">
+			<h1 class="remove-bottom" style="margin-top: 40px">Dan Rees&ndash;Jones</h1>
+			<address id="postal" class="remove-bottom"><strong>a: </strong>39 Slough Road, Datchet, Berks SL3 9AL</address>
+			<address id="email" class="remove-bottom"><strong>e: </strong>dan@rees-jones.co.uk</address>
+			<address id="phone"><strong>m: </strong>+44 (0) 7801 281027</address>
+		
+			<hr>
+		</header>
+		<div class="eight columns">
+			<h3>Previous Employment</h3>
+			<h4>2005&ndash;<?php echo date("Y"); ?>: Freelance web design & development</h4>
+			<h5>Recent highlighted work:</h5>
+			<ul>
+				<li>
+					<h5><a href="http://www.handmedownworld.co.uk" target="_blank">Hand Me Down World</a></h5>
+					<p>I helped  create the concept, then designed and developed this launch site for a John Murray publishing. Built on the CodeIgniter framework, the site utilised the Google Maps API to track the journey that one hundred pre-launch copies took between readers, who could then add their own reviews.</p>
+				</li>
+				<li>
+					<h5><a href="http://www.boxerbeetle.com" target="_blank">Boxer, Beetle</a></h5>
+					<p>A teaser site for Sceptre Publishing to garner interest in the author&#8217;s debut novel. I created the &#8216;artifacts&#8217;, and wrote custom jQuery around the scrollTo plugin to create the navigation.</p>
+				</li>
+				<li>
+					<h5><a href="http://fbanimations.tumblr.com/page/2" target="_blank">Facebook Animations</a></h5>
+					<p>This started as a night&#8217;s work making a single&ndash;serving Tumblr blog. It quickly went viral  and had gained over 100,000 unique visits within the first week.</p>
+				</li>
+				
+			</ul>
+		</div>
+		<div class="eight columns">
+			<h3>Education</h3>
+			<h4>1997&ndash;2000: Royal College of Music</h4>
+			<h4>1990&ndash;1996: Langley Grammar School</h4>
+			<p>A&ndash;levels: Computer Science, English Literature, Music, General Studies</p>
+		
+		</div>
+		<hr>
+		<div class="eight columns">
+			<h3>Skills</h3>
+			<h4>Strong</h4>
+			<ul class="disc">
+				<li>XHTML/CSS2</li>
+				<li>PHP/MySQL</li>
+				<li>Cross&ndash;platform, cross&ndash;browser compatibility</li>
+				<li>Excellent general IT skills and knowledge of both OSX and Windows</li>
+				<li>Outstanding written and spoken language skills</li>
+			</ul>
+			<h4>Familiar</h4>
+			<ul class="disc">
+				<li>HTML5/CSS3</li>
+				<li>JavaScript</li>
+				<li>jQuery</li>
+				<li>CSS3</li>
+				<li>CodeIgniter framework</li>
+			</ul>
+			<h4>Some Experience</h4>
+			<ul class="disc">
+				<li>Ruby on Rails</li>
+				<li>Version control using Git</li>
+			</ul>
+		</div>
+		<div class="eight columns">
+			<h3>Software</h3>
+			<h4>Strong</h4>
+			<ul class="disc">
+				<li>Adobe Dreamweaver (but I&#8217;d prefer to use TextMate)</li>
+				<li>Adobe Photoshop</li>
+				<li>Adobe Fireworks</li>
+			</ul>
+			<h4>Familiar</h4>
+			<ul class="disc">
+				<li>Adobe Illustrator</li>
+				<li>MS Office</li>
+			</ul>
+		</div>
+		<hr>
+		<div class="eight columns">
+			<h3>Interests</h3>
+			<ul class="disc">
+				<li>The Internet. <a href="http://www.youtube.com/watch?v=JcniyQYFU6M" target="_blank">I love it.</a></li>
+				<li>Playing the ukulele. I can play Sweet Child if you ask nicely.</li>
+				<li>Reading. My Kindle goes everywhere with me.</li>
+				<li>Trying to read everything in my InstaPaper account. It won&#8217;t stop growing.</li>
+				<li>Photography. I have a dSLR but most of my <a href="http://www.flickr.com/photos/revdan/" target="_blank">best photos</a> seem to be on my iPhone.</li>
+				<li>Cycling. I ride a fixed-gear because it&#8217;s fun, not because I&#8217;m trying to look cool.</li>
+				<li>Bizarre Heston Blumenthal-style kitchen inventions that invariably go wrong.</li>
+			</ul>
+		</div>
+		<div class="eight columns">
+			<h3>About Me</h3>
+			<p>Aside from everything internet&ndash;related, I&#8217;ve also been a professional bass player for ten years, playing bass in everything from covers bands to West End shows. It&#8217;s taken me all over the world and I&#8217;ve had some fantastic experiences. It&#8217;s time to settle down a bit now, though.</p>
+			<p>I&#8217;d consider myself a centre&ndash;brained person. I appreciate beautiful design just as much as beautiful code, and I&#8217;m equally as happy problem&ndash;solving as I am creating. I&#8217;m an autodidact, and most of my free time is spent learning something, whether it&#8217;s a new skill or language, or just having my mind blown by Wikipedia.</p>
+			
+			<p>I&#8217;m easy to get along with, but very persevering when presented with new challenges to overcome.</p>
+			 
+			<p>I think the most important thing in life is never to lose your sense of imagination and wonder. But it does help to have a finely&ndash;tuned sense of the ridiculous.</p>
+			
+		
+		</div>
+		<hr>
+		<footer>
+			
+		</footer>
+	</div><!-- container -->
+
+
+
+	<!-- JS
+	================================================== -->
+	<script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
+
+<!-- End Document
+================================================== -->
+</body>
+</html>
